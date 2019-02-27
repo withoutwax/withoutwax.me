29586952ea27f8d223306f82da01c3cfd0147785
Will Kim <rlagmlckd@gmail.com>
2019-02-27 14:13:56 -0600

    Created explanation page for withoutwax

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -9,27 +9,27 @@ import { Link } from 'gatsby';
 export class About extends React.Component {
     render() {
         return (
-            <div>
-                <Layout>
-                    <section style={{ textAlign:"center" }}>
-                        <h1>About</h1>
-                        <br />
-                        <h2>Hi. <span role="img" aria-label="cheers!">🙌🏼</span></h2>
-                        <p>My name is Will and I am an UX Designer and Engineer who also enjoys studying the boundaries of Computer Science <span role="img" aria-label="MacBookPro">💻</span>, UIUX and Product Design <span role="img" aria-label="Paint">🎨</span></p>
-                        <p>I also have special love for minimalism, technology and the nature <span role="img" aria-label="nature">🌱</span> Let's save the nature and make the <span role="img" aria-label="world">🌎</span> a better place.</p>
-                        <p>Plus, I love quotes too.</p>
-                        <p>Thank you for visiting!</p>
-                        <br />
-                        <br />
-                        <Link to="/" alt="without-wax">
-                            Without wax
-                        </Link>,
-                        <p>Will Kim</p>
-                    </section>
-                </Layout>
-            </div>
+            
+            <Layout>
+                <section style={{ textAlign:"center" }}>
+                    <h1>About</h1>
+                    <br />
+                    <h2 style={{ textTransform:"capitalize" }}>Hi. <span role="img" aria-label="cheers!">🙌🏼</span></h2>
+                    <p>My name is Will and I am an UX Designer and Engineer who also enjoys studying the boundaries of Computer Science <span role="img" aria-label="MacBookPro">💻</span>, UIUX and Product Design <span role="img" aria-label="Paint">🎨</span></p>
+                    <p>I also have special love for minimalism, technology and the nature <span role="img" aria-label="nature">🌱</span> Let's save the nature and make the <span role="img" aria-label="world">🌎</span> a better place.</p>
+                    <p>Plus, I love <a href="http://wordwithoutwax.tumblr.com/" rel="noopener noreferrer" target="_blank">quotes</a> too.</p>
+                    <p>Thank you for visiting!</p>
+                    <br />
+                    <br />
+                    <Link to="/about/withoutwax/" alt="withoutwax">
+                        without wax
+                    </Link>,
+                    <p>Will Kim</p>
+                </section>
+            </Layout>
+            
         );
     };
 }
 
-export default About
+export default About;
