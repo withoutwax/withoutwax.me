dbb01448b8f431e8a995e02c3f5d3794cd43b735
Will <rlagmlckd@gmail.com>
2019-05-28 18:00:20 +0900

    Configuring the site to use Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const cfg = {
   siteMetadata: {
     title: 'withoutwax.me',
     description: 'Hi 🙌🏼. My name is Will.',
@@ -92,4 +92,114 @@ module.exports = {
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
+};
+
+if (process.env.CONTEXT === "production") {
+  const googleAnalyticsCfg = {
+    resolve: "gatsby-plugin-google-analytics",
+    options: {
+      trackingId: "UA-45795165-1" // <- your tracking ID
+    }
+  };
+  cfg.plugins.push(googleAnalyticsCfg);
 }
+
+module.exports = cfg;
+
+// Old config =======
+
+// module.exports = {
+//   siteMetadata: {
+//     title: 'withoutwax.me',
+//     description: 'Hi 🙌🏼. My name is Will.',
+//   },
+//   plugins: [
+//     'gatsby-plugin-react-helmet',
+//     // {
+//     //   resolve: 'gatsby-plugin-sass',
+//     //   options: {
+
+//     //   }
+//     // },
+//     'gatsby-plugin-sass',
+//     'gatsby-plugin-styled-components',
+//     {
+//       // keep as first gatsby-source-filesystem plugin for gatsby image support
+//       resolve: 'gatsby-source-filesystem',
+//       options: {
+//         path: `${__dirname}/static/img`,
+//         name: 'uploads',
+//       },
+//     },
+//     {
+//       resolve: 'gatsby-source-filesystem',
+//       options: {
+//         path: `${__dirname}/src/pages`,
+//         name: 'pages',
+//       },
+//     },
+//     {
+//       resolve: 'gatsby-source-filesystem',
+//       options: {
+//         path: `${__dirname}/src/img`,
+//         name: 'images',
+//       },
+//     },
+//     `gatsby-transformer-yaml`,
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         path: `./src/data/`,
+//       },
+//     },
+//     'gatsby-plugin-sharp',
+//     'gatsby-transformer-sharp',
+//     {
+//       resolve: 'gatsby-transformer-remark',
+//       options: {
+//         plugins: [{
+//             resolve: 'gatsby-remark-relative-images',
+//             options: {
+//               name: 'uploads',
+//             },
+//           },
+//           {
+//             resolve: 'gatsby-remark-images',
+//             options: {
+//               // It's important to specify the maxWidth (in pixels) of
+//               // the content container as this plugin uses this as the
+//               // base for generating different widths of each image.
+//               maxWidth: 2048,
+//             },
+//           },
+//           {
+//             resolve: 'gatsby-remark-copy-linked-files',
+//             options: {
+//               destinationDir: 'static',
+//             },
+//           },
+//         ],
+//       },
+//     },
+//     {
+//       resolve: 'gatsby-plugin-netlify-cms',
+//       options: {
+//         modulePath: `${__dirname}/src/cms/cms.js`,
+//       },
+//     },
+//     {
+//       resolve: 'gatsby-plugin-favicon',
+//       options: {
+//         logo: "./src/img/favicon/favicon.png"
+//       }
+//     },
+//     {
+//       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+//       options: {
+//         develop: true, // Activates purging in npm run develop
+//         purgeOnly: ['/all.scss'], // applies purging only on the bulma css file
+//       },
+//     }, // must be after other CSS plugins
+//     'gatsby-plugin-netlify', // make sure to keep it last in the array
+//   ],
+// }
