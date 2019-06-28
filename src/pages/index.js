b392c53558a394fbd001fc11f8aa7cb0d60b390c
Will <rlagmlckd@gmail.com>
2019-06-28 10:40:35 +0900

    Changing the naming convention: UIUX -> Design

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,7 @@ export default () => (
                 <Links className="landing-container-link">
                     <StyledLink className="landing-container-link-link" to="/log">Log<span role="img" aria-label="camera">📷</span></StyledLink>
                     <StyledLink className="landing-container-link-link" to="/code">Code</StyledLink>
-                    <StyledLink className="landing-container-link-link" to="/uiux">UIUX</StyledLink>
+                    <StyledLink className="landing-container-link-link" to="/design">Design</StyledLink>
                     <StyledLink className="landing-container-link-link" to="lab">Lab<span role="img" aria-label="hammer">🔨</span></StyledLink>
                     <StyledLink className="landing-container-link-link" to="/about">Hi.<span role="img" aria-label="cheers">🙌🏼</span></StyledLink>
                 </Links>
