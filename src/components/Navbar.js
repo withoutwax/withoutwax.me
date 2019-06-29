82e6e01930ae95c3f0d1b8dea043fa93d7244698
Will <rlagmlckd@gmail.com>
2019-06-29 16:45:09 +0900

    Finalizing Responsiveness & Hamburger icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,80 +4,77 @@ import { Link } from 'gatsby'
 // import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
-  componentDidMount() {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll('.navbar-burger'),
-      0
-    )
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      // Add a click event on each of them
-      $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target
-          const $target = document.getElementById(target)
-
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active')
-          $target.classList.toggle('is-active')
-        })
-      })
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false,
+      navBarActiveClass: '',
     }
+  }
+
+  toggleHamburger = () => {
+    // toggle the active boolean in the state
+    this.setState(
+      {
+        active: !this.state.active,
+      },
+      // after state has been updated,
+      () => {
+        // set the class in state for the navbar accordingly
+        this.state.active
+          ? this.setState({
+              navBarActiveClass: 'is-active',
+            })
+          : this.setState({
+              navBarActiveClass: '',
+            })
+      }
+    )
   }
 
   render() {
     return (
       <nav
-        className="navbar is-transparent"
+        className="navbar"
         role="navigation"
         aria-label="main-navigation"
       >
-        <div className="nav-container">
-          <div id="navMenu" className="navbar-menu">
-            <div className="navbar-start has-text-centered">
-              
-              <Link className="navbar-item" activeClassName="active" to="/blog" style={{ fontWeight:"bold" }} >
-                All
-              </Link>
-              <div className="navbar-sub">
-                <Link className="navbar-item" to="/log" activeClassName="active">
-                Log<span role="img" aria-label="camera">📷</span>
-                </Link>
-                <Link className="navbar-item" to="/code" activeClassName="active">
-                  Code
-                </Link>
-                <Link className="navbar-item" to="/design" activeClassName="active">
-                  Design
-                </Link>
-                <Link className="navbar-item" to="/lab" activeClassName="active">
-                  Lab<span role="img" aria-label="hammer">🔨</span>
-                </Link>
-              </div>
-              <Link className="navbar-item" to="/archive" activeClassName="active">
-                <span role="img" aria-label="archive">🗄</span>
-              </Link>
-              <Link className="navbar-item" to="/about" activeClassName="active">
-                About
-              </Link>
-              <Link className="navbar-item" to="/contact" activeClassName="active">
-                Contact
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              {/* <a
-                className="navbar-item"
-                href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a> */}
-            </div>
+        {/* <div className="hamburger-menu"  >{this.state.Logo}</div> */}
+
+        {/* Hamburger Button */}
+        <button class={`hamburger hamburger--slider ${this.state.navBarActiveClass}`} onClick={() => this.toggleHamburger()} type="button">
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
+
+        <div className={`nav-container ${this.state.navBarActiveClass}`}>
+          <Link className="navbar-item" activeClassName="active" to="/blog" style={{ fontWeight:"bold" }} >
+            All
+          </Link>
+          <div className="navbar-sub">
+            <Link className="navbar-item" to="/log" activeClassName="active">
+            Log<span role="img" aria-label="camera">📷</span>
+            </Link>
+            <Link className="navbar-item" to="/code" activeClassName="active">
+              Code
+            </Link>
+            <Link className="navbar-item" to="/design" activeClassName="active">
+              Design
+            </Link>
+            <Link className="navbar-item" to="/lab" activeClassName="active">
+              Lab<span role="img" aria-label="hammer">🔨</span>
+            </Link>
           </div>
+          <Link className="navbar-item" to="/archive" activeClassName="active">
+            <span role="img" aria-label="archive">🗄</span>
+          </Link>
+          <Link className="navbar-item" to="/about" activeClassName="active">
+            About
+          </Link>
+          <Link className="navbar-item" to="/contact" activeClassName="active">
+            Contact
+          </Link>
         </div>
       </nav>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -72,6 +72,7 @@ export default () => (
         <Helmet>
             <meta charSet="utf-8" />
             <title>Will's Blog</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         </Helmet>
         <LandingPage>
             <Logo src={logo} alt="logo" />
