1e4134f2b9bf8746c18c5d91e1d449620fd5baa5
Will <rlagmlckd@gmail.com>
2020-09-09 12:16:45 +0900

    Updating Blog category pill design

--- src/components/BlogRollCategory.js
+++ src/components/BlogRollCategory.js
@@ -5,42 +5,42 @@ export default class BlogRollCategory extends React.Component {
     super(props)
     this.state = {
       category: this.props.category,
-      componentClass: 'inline-flex items-center mb-4 px-3 py-1 rounded-full text-xs font-medium leading-4 bg-gray-200 text-gray-800',
+      componentClass: 'inline-flex items-center mb-4 py-1 rounded-full text-sm font-medium leading-4',
       emoji: '',
     }
   }
 
   componentDidMount(){
-    let color = '';
+    // let color = '';
     switch (this.state.category) {
       case 'Thoughts':
         this.setState({
           emoji: '💭'
         });
-        color = 'orange';
+        // color = 'orange';
         break;
       case 'Project':
         this.setState({
           emoji: '🕹'
         });
-        color = 'red';
+        // color = 'red';
         break;
       case 'Lab':
         this.setState({
           emoji: '🔨'
         });
-        color = 'purple';
+        // color = 'purple';
         break;
       default:
         this.setState({
           emoji: '💻'
         });
-        color = 'gray';
+        // color = 'gray';
         break;
     }
-    this.setState({
-      componentClass: `bg-${color}-200 text-${color}-800 inline-flex items-center mb-4 px-3 py-1 rounded-full text-xs font-medium leading-4`,
-    });
+    // this.setState({
+    //   componentClass: `bg-${color}-200 text-${color}-800 inline-flex items-center mb-4 px-3 py-1 rounded-full text-xs font-medium leading-4`,
+    // });
   }
 
   render() {
