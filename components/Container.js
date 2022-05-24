991e8527d735a31a4a272b66d4abab49ab383fe8
Will <rlagmlckd@gmail.com>
2022-05-24 20:14:16 +0900

    Removing using hooks for incompatibility with Preact
    - https://github.com/preactjs/preact/pull/3510
    - https://stackoverflow.com/questions/71959778/error-in-production-after-upgrading-to-react-18
    
    Signed-off-by: Will <rlagmlckd@gmail.com>

--- components/Container.js
+++ components/Container.js
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import NextLink from 'next/link';
 
 import Footer from '@/components/Footer';
 
 export default function Container({ children }) {
-  const [mounted, setMounted] = useState(false);
+  // const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
   // After mounting, we have access to the theme
-  useEffect(() => setMounted(true), []);
+  // useEffect(() => setMounted(true), []);
 
   return (
     <div className="flex flex-col md:flex-row mx-auto bg-white dark:bg-black md:bg-transparent w-full md:w-auto max-w-6xl">
@@ -71,31 +71,31 @@ export default function Container({ children }) {
             className="order-0 md:order-1 bg-gray-200 dark:bg-gray-800 rounded mr-6 md:m-2 p-3 h-10 w-10"
             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
           >
-            {mounted && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                stroke="currentColor"
-                className="h-4 w-4 text-gray-800 dark:text-gray-200"
-              >
-                {theme === 'dark' ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                  />
-                )}
-              </svg>
-            )}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="currentColor"
+              className="h-4 w-4 text-gray-800 dark:text-gray-200"
+            >
+              {theme === 'dark' ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              )}
+            </svg>
+            {/* {mounted && (
+            )} */}
           </button>
         </div>
       </nav>
