264d98e97ad1f741a60fff1911ed5ebdb26ea705
Will <rlagmlckd@gmail.com>
2022-08-05 21:08:04 +0900

    Updating layout for mobile
    Signed-off-by: Will <rlagmlckd@gmail.com>

--- pages/code.js
+++ pages/code.js
@@ -34,7 +34,7 @@ export default function Blog({ posts }) {
           description
         }}
       />
-      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+      <div className="flex flex-col justify-center items-start max-w-2xl w-full mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Code 💻
         </h1>
