b0a021f46519a6fe000b471e1e5e6a5d08b5dd64
Will Kim <rlagmlckd@gmail.com>
2024-09-28 01:49:14 +0900

    Making the entire screen to match the light / dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,74 +30,75 @@ export default function RootLayout({
       </head>
       <body className={`${inter.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="light">
-          {/* {children} */}
-          <div className="mx-auto flex w-full max-w-[900px] flex-col bg-white dark:bg-black md:flex-row md:bg-transparent">
-            <nav className="navbar sticky top-0 z-10 my-0 flex w-full flex-wrap items-center justify-between gap-8 bg-white bg-opacity-50 px-8 py-8 dark:bg-black md:h-screen md:w-2/12 md:flex-col md:gap-0 md:py-16">
-              <div className="md:order-0 order-1 flex w-full justify-between md:flex-col">
-                <Link
-                  href="/"
-                  className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  <span role="img" aria-label="home">
-                    🏡
-                  </span>
-                </Link>
-                <Link
-                  href="/blog"
-                  className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  <span role="img" aria-label="thoughts">
-                    💭
-                  </span>
-                </Link>
-                <Link
-                  href="/engineering"
-                  className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  <span role="img" aria-label="code">
-                    💻
-                  </span>
-                </Link>
-                <Link
-                  href="/project"
-                  className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  <span role="img" aria-label="project">
-                    🕹
-                  </span>
-                </Link>
-                <Link
-                  href="/about"
-                  className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  <span role="img" aria-label="profile">
-                    👨🏻‍💻
-                  </span>
-                </Link>
-                <Link
-                  href="/contact"
-                  className="md:order-0 order-1 p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  <span role="img" aria-label="mail">
-                    📬
-                  </span>
-                </Link>
-              </div>
+          <div className="w-full dark:bg-black">
+            <div className="mx-auto flex w-full max-w-[900px] flex-col bg-white  md:flex-row md:bg-transparent">
+              <nav className="navbar sticky top-0 z-10 my-0 flex w-full flex-wrap items-center justify-between gap-8 bg-white bg-opacity-50 px-8 py-8 dark:bg-black md:h-screen md:w-2/12 md:flex-col md:gap-0 md:py-16">
+                <div className="md:order-0 order-1 flex w-full justify-between md:flex-col">
+                  <Link
+                    href="/"
+                    className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
+                  >
+                    <span role="img" aria-label="home">
+                      🏡
+                    </span>
+                  </Link>
+                  <Link
+                    href="/blog"
+                    className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
+                  >
+                    <span role="img" aria-label="thoughts">
+                      💭
+                    </span>
+                  </Link>
+                  <Link
+                    href="/engineering"
+                    className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
+                  >
+                    <span role="img" aria-label="code">
+                      💻
+                    </span>
+                  </Link>
+                  <Link
+                    href="/project"
+                    className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
+                  >
+                    <span role="img" aria-label="project">
+                      🕹
+                    </span>
+                  </Link>
+                  <Link
+                    href="/about"
+                    className="p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
+                  >
+                    <span role="img" aria-label="profile">
+                      👨🏻‍💻
+                    </span>
+                  </Link>
+                  <Link
+                    href="/contact"
+                    className="md:order-0 order-1 p-1 text-center text-xl text-gray-900 dark:text-gray-100 sm:p-4"
+                  >
+                    <span role="img" aria-label="mail">
+                      📬
+                    </span>
+                  </Link>
+                </div>
 
-              {/* Desktop - Dark Mode Button */}
-              <div className="order-0 hidden items-center justify-center md:order-1 md:flex">
-                <DarkModeButton />
-              </div>
-            </nav>
-            <main className="mb-8 flex w-full flex-col bg-white p-8 dark:bg-black md:mt-8 md:w-10/12">
-              {children}
-              <Footer />
-            </main>
-          </div>
+                {/* Desktop - Dark Mode Button */}
+                <div className="order-0 hidden items-center justify-center md:order-1 md:flex">
+                  <DarkModeButton />
+                </div>
+              </nav>
+              <main className="mb-8 flex w-full flex-col bg-white p-8 dark:bg-black md:mt-8 md:w-10/12">
+                {children}
+                <Footer />
+              </main>
+            </div>
 
-          {/* Mobile - Dark Mode Button */}
-          <div className="fixed bottom-8 left-8 md:hidden">
-            <DarkModeButton />
+            {/* Mobile - Dark Mode Button */}
+            <div className="fixed bottom-8 left-8 md:hidden">
+              <DarkModeButton />
+            </div>
           </div>
         </ThemeProvider>
       </body>
