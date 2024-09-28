2e318a1d408d69e63b5a9e8e953e7d08874a2dca
Will Kim <rlagmlckd@gmail.com>
2024-09-29 01:13:58 +0900

    Setting up Lists

--- components/CustomNotionBlockRenderer.tsx
+++ components/CustomNotionBlockRenderer.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { JSX, useState } from "react";
+import Image from "next/image";
 
 interface Block {
   type: string;
@@ -17,30 +18,284 @@ export default function CustomNotionBlockRenderer({
   blockMap,
 }: CustomNotionBlockRendererProps) {
   console.log("Block Map", blockMap);
-  return (
-    <div>
-      {blockMap.map((block, index) => (
-        <BlockBuilder key={index} block={block} />
-      ))}
-    </div>
-  );
+  let olList: JSX.Element[] = [];
+  let ulList: JSX.Element[] = [];
+
+  const element = blockMap.map((block, index) => {
+    let returnElement: JSX.Element[] = [];
+
+    if (block.type !== "numbered_list_item" && olList.length > 0) {
+      const list = olList;
+      olList = [];
+      returnElement.push(
+        <ol key={index} className="list-decimal list-inside space-y-1 ml-3">
+          {list}
+        </ol>
+      );
+    } else if (block.type !== "bulleted_list_item" && ulList.length > 0) {
+      const list = ulList;
+      ulList = [];
+      returnElement.push(
+        <ul key={index} className="list-disc list-inside space-y-1 ml-3">
+          {list}
+        </ul>
+      );
+    }
+
+    switch (block.type) {
+      case "heading_1":
+        returnElement.push(<Heading1 block={block} />);
+        break;
+      case "heading_2":
+        returnElement.push(<Heading2 block={block} />);
+        break;
+      case "heading_3":
+        returnElement.push(<Heading3 block={block} />);
+        break;
+      case "paragraph":
+        returnElement.push(<Paragraph block={block} />);
+        break;
+      case "numbered_list_item":
+        olList.push(<ListElement block={block} />);
+        break;
+      case "bulleted_list_item":
+        ulList.push(<ListElement block={block} />);
+        break;
+      case "image":
+        returnElement.push(<ImageBlock block={block} />);
+        break;
+      case "code":
+        returnElement.push(<Code block={block} />);
+        break;
+      default:
+        returnElement.push(<></>);
+        break;
+    }
+
+    return [...returnElement];
+  });
+
+  return <div className="space-y-3">{element}</div>;
 }
 
-const BlockBuilder = ({ block }: { block: Block }) => {
-  switch (block.type) {
-    case "paragraph":
-      return <Paragraph block={block} />;
-    // Add more cases for different block types as needed
-    default:
-      return <></>;
-  }
+const ListElement = (block: any) => {
+  console.log("List Element", block);
+  const lists =
+    block.block.type === "numbered_list_item"
+      ? block.block.numbered_list_item.rich_text
+      : block.block.bulleted_list_item.rich_text;
+  const elements = lists.map((element: any) => {
+    return (
+      <li key={element.id}>
+        <span
+          className={`${element.annotations.bold && "font-bold"} ${
+            element.annotations.italic && "italic"
+          } ${element.annotations.strikethrough && "line-through"} ${
+            element.annotations.underline && "underline"
+          } ${
+            element.annotations.code &&
+            "font-mono text-red-500 bg-[#eeeeec] dark:bg-gray-800 px-1 rounded-sm"
+          } ${
+            element.annotations.color &&
+            notionBackgroundColor(element.annotations.color)
+          } ${
+            element.annotations.color &&
+            notionTextColor(element.annotations.color)
+          }`}
+          key={element.id}
+        >
+          {element.text.content}
+        </span>
+      </li>
+    );
+  });
+  return elements.length > 0 ? elements : <></>;
+};
+
+const ImageBlock = (block: any) => {
+  console.log("Image block", block);
+  const caption = block.block.image.caption.map((element: any) => {
+    return (
+      <p className="text-gray-600 dark:text-gray-400 text-xs">
+        {element.text.content}
+      </p>
+    );
+  });
+  return (
+    <div>
+      <Image
+        src={block.block.image.file.url}
+        // alt={block.block.image.caption[0].text.content}
+        alt={``}
+        width={700}
+        height={500}
+        // width={block.block.image.file.width}
+        // height={block.block.image.file.height}
+        layout="responsive"
+        className="w-full h-full object-cover"
+      />
+      <p className="text-gray-600 dark:text-gray-400 text-xs">{caption}</p>
+    </div>
+  );
+};
+
+const Code = (block: any) => {
+  console.log("Code Block", block);
+  const elements = block.block.code.rich_text.map((element: any) => {
+    return (
+      <div className="prose">
+        <pre className="relative ">
+          <code>{element.text.content}</code>
+          {/* <p className="absolute right-4 text-gray-400">
+            {block.block.code.language}
+          </p> */}
+        </pre>
+      </div>
+    );
+  });
+  const caption = block.block.code.caption.map((element: any) => {
+    return (
+      <p className="text-gray-600 dark:text-gray-400 text-xs">
+        {element.text.content}
+      </p>
+    );
+  });
+  return (
+    <>
+      {elements.length > 0 ? (
+        <div>
+          {elements}
+          {caption}
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+const Heading1 = (block: any) => {
+  // console.log("Heading 1", block);
+  const elements = block.block.heading_1.rich_text.map((element: any) => {
+    if (element.href) {
+      return (
+        <a
+          href={element.text.link.url}
+          className={`text-blue-500 underline`}
+          target="_blank"
+          rel="noreferrer noopener"
+          key={element.id}
+        >
+          <h3 className={`text-4xl font-bold`}>{element.text.content}</h3>
+        </a>
+      );
+    }
+    return (
+      <h3
+        className={`text-4xl font-bold ${
+          element.annotations.bold && "font-bold"
+        } ${element.annotations.italic && "italic"} ${
+          element.annotations.strikethrough && "line-through"
+        } ${element.annotations.underline && "underline"} ${
+          element.annotations.code &&
+          "font-mono text-red-500 bg-[#eeeeec] dark:bg-gray-800 px-1 rounded-sm"
+        } ${
+          element.annotations.color &&
+          notionBackgroundColor(element.annotations.color)
+        } ${
+          element.annotations.color &&
+          notionTextColor(element.annotations.color)
+        }`}
+      >
+        {element.text.content}
+      </h3>
+    );
+  });
+  return <>{elements.length > 0 ? elements : <h3></h3>}</>;
+};
+const Heading2 = (block: any) => {
+  // console.log("Heading 2", block);
+  const elements = block.block.heading_2.rich_text.map((element: any) => {
+    if (element.href) {
+      return (
+        <a
+          href={element.text.link.url}
+          className={`text-blue-500 underline`}
+          target="_blank"
+          rel="noreferrer noopener"
+          key={element.id}
+        >
+          <h3 className={`text-3xl font-bold`}>{element.text.content}</h3>
+        </a>
+      );
+    }
+    return (
+      <h3
+        className={`text-3xl font-bold ${
+          element.annotations.bold && "font-bold"
+        } ${element.annotations.italic && "italic"} ${
+          element.annotations.strikethrough && "line-through"
+        } ${element.annotations.underline && "underline"} ${
+          element.annotations.code &&
+          "font-mono text-red-500 bg-[#eeeeec] dark:bg-gray-800 px-1 rounded-sm"
+        } ${
+          element.annotations.color &&
+          notionBackgroundColor(element.annotations.color)
+        } ${
+          element.annotations.color &&
+          notionTextColor(element.annotations.color)
+        }`}
+      >
+        {element.text.content}
+      </h3>
+    );
+  });
+  return <>{elements.length > 0 ? elements : <h3></h3>}</>;
+};
+const Heading3 = (block: any) => {
+  // console.log("Heading 3", block);
+  const elements = block.block.heading_3.rich_text.map((element: any) => {
+    if (element.href) {
+      return (
+        <a
+          href={element.text.link.url}
+          className={`text-blue-500 underline`}
+          target="_blank"
+          rel="noreferrer noopener"
+          key={element.id}
+        >
+          <h3 className={`text-xl font-bold`}>{element.text.content}</h3>
+        </a>
+      );
+    }
+    return (
+      <h3
+        className={`text-xl font-bold ${
+          element.annotations.bold && "font-bold"
+        } ${element.annotations.italic && "italic"} ${
+          element.annotations.strikethrough && "line-through"
+        } ${element.annotations.underline && "underline"} ${
+          element.annotations.code &&
+          "font-mono text-red-500 bg-[#eeeeec] dark:bg-gray-800 px-1 rounded-sm"
+        } ${
+          element.annotations.color &&
+          notionBackgroundColor(element.annotations.color)
+        } ${
+          element.annotations.color &&
+          notionTextColor(element.annotations.color)
+        }`}
+      >
+        {element.text.content}
+      </h3>
+    );
+  });
+  return <>{elements.length > 0 ? elements : <h3></h3>}</>;
 };
 
 const Paragraph = (block: any) => {
-  console.log("Paragraph Block", block.block);
-
+  // console.log("Paragraph Block", block.block);
   const elements = block.block.paragraph.rich_text.map((element: any) => {
-    console.log("Element", element);
+    // console.log("Element", element);
     if (element.href) {
       return (
         <a
@@ -81,7 +336,7 @@ const Paragraph = (block: any) => {
     );
   });
 
-  return <p>{elements.length > 0 ? elements : <p>Empty values</p>}</p>;
+  return <p>{elements.length > 0 ? elements : <p></p>}</p>;
 };
 
 const notionBackgroundColor = (color: string) => {
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["prod-files-secure.s3.us-west-2.amazonaws.com"],
+  },
+};
 
 export default nextConfig;
