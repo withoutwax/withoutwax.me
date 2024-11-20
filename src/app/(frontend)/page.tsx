37b77632498e90e6175408df7c99a7ad9e1bc2c6
Will Kim <rlagmlckd@gmail.com>
2024-11-21 02:51:48 +0900

    Configuring simple data fetch from the backend

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,15 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { getPayload } from 'payload';
+import config from '@payload-config';
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayload({ config });
+  const test = await payload.find({
+    collection: 'tests',
+  });
+
+  console.log('test', test);
+
   return (
     <div className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
       <h1 className="mb-4 w-full text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
@@ -22,7 +31,7 @@ export default function Home() {
         Blender
       </h2>
       <p className="mb-4 w-full text-gray-600 dark:text-gray-400">
-        For anyone who came from this{" "}
+        For anyone who came from this{' '}
         <a
           href="https://www.youtube.com/user/Calibre097/featured"
           className="text-gray-900 underline hover:text-gray-500 dark:text-gray-100"
@@ -31,9 +40,8 @@ export default function Home() {
         >
           YouTube channel
         </a>
-        , a lot of things have changed: I kept following my passion, and now I
-        am a Software Developer. If you are interested in my story, I&apos;ve
-        shared my story
+        , a lot of things have changed: I kept following my passion, and now I am a Software
+        Developer. If you are interested in my story, I&apos;ve shared my story
         <Link
           href="/blog/my-story"
           className="ml-1 text-gray-900 underline hover:text-gray-500 dark:text-gray-100"
@@ -50,10 +58,7 @@ export default function Home() {
         <p className="mb-4 text-gray-600 dark:text-gray-400">
           For any old posts before ~2018, you can find them here:
           <span>
-            <Link
-              href="/archive"
-              className="p-1 text-xl text-gray-900 dark:text-gray-100 sm:p-4"
-            >
+            <Link href="/archive" className="p-1 text-xl text-gray-900 dark:text-gray-100 sm:p-4">
               <span role="img" aria-label="archive">
                 🗄
               </span>
