22954e3afc628a6a4393dfe62f1f8c5a89a1d8f2
Will Kim <rlagmlckd@gmail.com>
2024-11-21 17:37:45 +0900

    Updating this so that the page can revalidate properly for global pages

--- src/hooks/revalidatePage.ts
+++ src/hooks/revalidatePage.ts
@@ -1,12 +1,9 @@
 import type { GlobalAfterChangeHook } from 'payload';
-
 import { revalidatePath } from 'next/cache';
-
-import type { Blog, Code, Project, Archive } from '@/payload-types';
 
 export const revalidatePage: GlobalAfterChangeHook = ({ doc, previousDoc, req: { payload } }) => {
   if (JSON.stringify(doc) !== JSON.stringify(previousDoc)) {
-    const path = `/${doc.slug}`;
+    const path = `/${previousDoc.globalType}`;
 
     payload.logger.info(`Revalidating post at path: ${path}`);
 
