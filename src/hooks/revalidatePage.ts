1f68f49e82f7207f7e3657501cb96d5f115c4abe
Will Kim <rlagmlckd@gmail.com>
2024-11-21 17:25:06 +0900

    Changing this to detect page update

--- src/hooks/revalidatePage.ts
+++ src/hooks/revalidatePage.ts
@@ -5,21 +5,12 @@ import { revalidatePath } from 'next/cache';
 import type { Blog, Code, Project, Archive } from '@/payload-types';
 
 export const revalidatePage: GlobalAfterChangeHook = ({ doc, previousDoc, req: { payload } }) => {
-  if (doc._status === 'published') {
+  if (JSON.stringify(doc) !== JSON.stringify(previousDoc)) {
     const path = `/${doc.slug}`;
 
     payload.logger.info(`Revalidating post at path: ${path}`);
 
     revalidatePath(path);
-  }
-
-  // If the post was previously published, we need to revalidate the old path
-  if (previousDoc._status === 'published' && doc._status !== 'published') {
-    const oldPath = `/${previousDoc.slug}`;
-
-    payload.logger.info(`Revalidating old post at path: ${oldPath}`);
-
-    revalidatePath(oldPath);
   }
 
   return doc;
