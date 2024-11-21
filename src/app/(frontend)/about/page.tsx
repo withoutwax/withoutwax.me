81273cb457994b95830bb28c72257cb6bb368d9b
Will Kim <rlagmlckd@gmail.com>
2024-11-21 18:44:10 +0900

    Using ORM to fetch data - following the recommended way

--- src/app/(frontend)/about/page.tsx
+++ src/app/(frontend)/about/page.tsx
@@ -2,17 +2,30 @@ import type { Metadata } from 'next';
 import { getPayload } from 'payload';
 import config from '@payload-config';
 import RichText from '@/components/RichText';
+import { unstable_cache } from 'next/cache';
 
 export const metadata: Metadata = {
   title: "Will's Blog | About",
   description: 'About Will Kim',
 };
 
-export default async function About() {
-  const payload = await getPayload({ config });
-  const data = await payload.findGlobal({
-    slug: 'about',
-  });
+const getCachedData = unstable_cache(
+  async () => {
+    const payload = await getPayload({ config });
+    return await payload.findGlobal({
+      slug: 'about',
+    });
+  },
+  ['about'],
+  { revalidate: 3600, tags: ['about'] },
+);
 
-  return <RichText content={data.content} />;
+export default async function About() {
+  const data = getCachedData();
+
+  if (!data) {
+    return <></>;
+  }
+
+  return <RichText content={(await data).content} />;
 }
