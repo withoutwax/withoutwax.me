449925675f66a1642d54bf4eca7f06731fbdd501
Will Kim <rlagmlckd@gmail.com>
2026-01-01 18:46:12 +0900

    feat: Cover Letter 추가

--- app/cover-letter/CoverLetterContainer.tsx
+++ app/cover-letter/CoverLetterContainer.tsx
@@ -21,7 +21,7 @@ const CoverLetterClient = () => {
         <span role="img" aria-label="download" className="text-3xl">
           📄
         </span>
-        <span className="text-sm font-bold text-gray-500 hover:text-gray-700 dark:text-gray-100">
+        <span className="text-sm font-bold text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-gray-200 transition-colors">
           PDF로 다운로드
         </span>
       </button>
